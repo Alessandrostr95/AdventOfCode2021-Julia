4b68803a1eee5cd37ac374ff128421bda9835b1b
alessandro <alessandro@pop-os.localdomain>
2021-12-08 22:39:20 +0100

    DAY 3

--- 03/day3.jl
+++ 03/day3.jl
@@ -1,8 +1,8 @@
-report = (read("input.txt", String) |> (x -> split(x, "\n")))[1:end-1]
+report = (read("input.txt", String) |> (x -> split(x, "\n")))
+filter!(x -> x ≠ "", report)
 len = length(first(report))
 
 γ, ε = "", ""
-
 
 for i=1:len
     ones_count = 0
@@ -27,3 +27,40 @@ end
 println("The γ rate is $γ")
 println("The ε rate is $ε")
 println("The power consumption is $(γ * ε)")
+
+
+###### Part 2
+oxygen, CO₂ = copy(report), copy(report)
+
+k = 1
+while length(oxygen) > 1
+    global k
+    count₁ = 0
+    for bit_sequence ∈ oxygen
+        if bit_sequence[k] == '1'
+            count₁ += 1
+        end
+    end
+    majority_bit = count₁ ≥ length(oxygen)/2  ? '1' : '0'
+    filter!(x -> x[k] == majority_bit, oxygen)
+    k += 1
+end
+
+
+k = 1
+while length(CO₂) > 1
+    global k
+    count₀ = 0
+    for bit_sequence ∈ CO₂
+        if bit_sequence[k] == '0'
+            count₀ += 1
+        end
+    end
+    minority_bit = count₀ ≤ length(CO₂)/2 ? '0' : '1'
+    filter!(x -> x[k] == minority_bit, CO₂)
+    k += 1
+end
+
+println("oxigen = $(parse(Int64, first(oxygen), base=2))")
+println("CO₂ = $(parse(Int64, first(CO₂), base=2))")
+println("final report = $(parse(Int64, first(oxygen), base=2) * parse(Int64, first(CO₂), base=2))")
