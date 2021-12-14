fbeea476faae6a295477f3d1c3414dd9b9b29bac
Alessandrostr95 <alessandrostr95@gmail.com>
2021-12-14 02:03:44 +0100

    [AoC] day 5 part 2

--- 05/day5.jl
+++ 05/day5.jl
@@ -34,3 +34,49 @@ end
 println(A)
 n = count(x -> x ≥ 2, A)
 println("There are $n points where at least two lines overlap.")
+
+# --- Part Two ---
+diagonal_lines = Pair{Tuple{Int64, Int64}, Tuple{Int64, Int64}}[]
+
+function is_diagonal(l::Vector{Int64})::Bool
+    p1, p2 = l[1:2], l[3:4]
+    return abs(p1[1] - p2[1]) == abs(p1[2] - p2[2])
+end
+
+for l ∈ f
+    global diagonal_lines, xs, ys
+    coord = replace(l, "->" => " ") |> (x -> replace(x, "," => " ")) |> split .|> (x -> parse(Int64, x))
+    if is_diagonal(coord)
+        push!(diagonal_lines, (coord[1], coord[2]) => (coord[3], coord[4]))
+    end
+end
+
+for l ∈ diagonal_lines
+    p1, p2 = l
+    Δ = abs(p1[1] - p2[1])
+
+    if p1[1] < p2[1] && p1[2] < p2[2]
+        for i=0:Δ
+            A[p1[1] + i, p1[2] + i] += 1
+        end
+
+    elseif p1[1] > p2[1] && p1[2] > p2[2]
+        for i=0:Δ
+            A[p1[1] - i, p1[2] - i] += 1
+        end
+
+    elseif p1[1] < p2[1] && p1[2] > p2[2]
+        for i=0:Δ
+            A[p1[1] + i, p1[2] - i] += 1
+        end
+
+    elseif p1[1] > p2[1] && p1[2] < p2[2]
+        for i=0:Δ
+            A[p1[1] - i, p1[2] + i] += 1
+        end
+    end
+end
+
+println(A)
+n = count(x -> x ≥ 2, A)
+println("There are $n points where at least two lines overlap.")
